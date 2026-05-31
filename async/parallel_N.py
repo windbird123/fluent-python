9f12c79703e587e48252646284249d5712444783
jaemyung.kim <jaemyung.kim@navercorp.com>
2026-05-31 16:59:06 +0900

    update async

--- async/parallel_N.py
+++ async/parallel_N.py
@@ -12,9 +12,23 @@ async def run_task(x):
         return x * 2
 
 
-async def main():
+async def main_old():
     coros = [run_task(x) for x in tasks]
     await asyncio.gather(*coros)
 
+async def main():
+    tracks = []
+
+    try:
+        async with asyncio.TaskGroup() as tg:
+            for x in tasks:
+                t = tg.create_task(run_task(x))
+                tracks.append(t)
+    except* Exception as eg:
+        for exc in eg.exceptions:
+            print(f"task 예외: {exc!r}")
+
+    results = [t.result() for t in tracks if not t.cancelled() and t.exception() is None]
+    print(results)
 
 asyncio.run(main())
